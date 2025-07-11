201c6f049c3ef936eb197436ef74773f5e4a16b3
Roland Lopez <rolandlopez.developer@gmail.com>
2025-07-11 13:39:31 +0700

    Enable UUID

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_11_061053) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_11_060352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "pgcrypto"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "conent"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
